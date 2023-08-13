d7ae87af48d5b3cfd0f7cb05b7f34c31cbae1695
maojiu <maojiu-bb@outlook.com>
2023-08-13 10:45:14 +0800

    feat -> add dio

--- lib/common/values/constants.dart
+++ lib/common/values/constants.dart
@@ -1,7 +1,8 @@
 /// 常量
 class Constants {
   // 服务 api
-  static const apiUrl = 'https://api.example.com';
+  // static const apiUrl = 'https://api.example.com';
+  static const wpApiBaseUrl = 'https://wpapi.ducafecat.tech';
 
   // 本地存储key
   // 离线后保存语言种类
--- lib/global.dart
+++ lib/global.dart
@@ -7,13 +7,16 @@ class Global {
     // flutter 跟原生端的接口进行初始化
     WidgetsFlutterBinding.ensureInitialized();
 
-    // storage 工具类
-    // 持久化存储
-    await Storage().init();
-
     // 注入全局config service
     await Future.wait([
       Get.putAsync<ConfigService>(() async => await ConfigService().init()),
     ]).whenComplete(() {});
+
+    // storage 工具类
+    // 持久化存储
+    await Storage().init();
+
+    // dio service
+    Get.put<WPHttpService>(WPHttpService());
   }
 }
