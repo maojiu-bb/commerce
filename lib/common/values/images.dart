2ee46d1b55489cf6cd15899096eba071073bb70a
maojiu <maojiu-bb@outlook.com>
2023-08-13 16:23:18 +0800

    feat -> add images and svgs

--- lib/common/values/images.dart
+++ lib/common/values/images.dart
@@ -1,3 +1,9 @@
 /// 图片 assets
 class AssetsImages {
+  static const splashPng = 'assets/images/Splash.png';
+  static const homePlaceholder_1Png = 'assets/images/home_placeholder 1.png';
+  static const orderConfirmedPng = 'assets/images/order_confirmed.png';
+  static const welcome_1Png = 'assets/images/welcome_1.png';
+  static const welcome_2Png = 'assets/images/welcome_2.png';
+  static const welcome_3Png = 'assets/images/welcome_3.png';
 }
