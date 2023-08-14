abcba352149ba08c4de1949402535c85ef2aacf0
maojiu <maojiu-bb@outlook.com>
2023-08-14 23:07:30 +0800

    feat -> 错误全局处理

--- lib/common/models/index.dart
+++ lib/common/models/index.dart
@@ -1,6 +1,7 @@
 library models;
 
 export './welcome_model.dart';
+export './error_message_model.dart';
 
 export './request/user_register_req.dart';
 // export './xxxx.dart';
--- lib/common/services/wp_http.dart
+++ lib/common/services/wp_http.dart
@@ -133,10 +133,11 @@ class RequestInterceptors extends Interceptor {
   }
 
   /// 退出并重新登录
-  // Future<void> _errorNoAuthLogout() async {
-  //   await UserService.to.logout();
-  //   Get.toNamed(RouteNames.systemLogin);
-  // }
+  // 退出并重新登录
+  Future<void> _errorNoAuthLogout() async {
+    // await UserService.to.logout();
+    Get.toNamed(RouteNames.systemLogin);
+  }
 
   @override
   Future<void> onError(
@@ -147,22 +148,22 @@ class RequestInterceptors extends Interceptor {
     switch (err.type) {
       case DioExceptionType.badResponse: // 服务端自定义错误体处理
         {
-          // final response = err.response;
-          // final errorMessage = ErrorMessageModel.fromJson(response?.data);
-          // switch (errorMessage.statusCode) {
-          //   case 401:
-          //     _errorNoAuthLogout();
-          //     break;
-          //   case 404:
-          //     break;
-          //   case 500:
-          //     break;
-          //   case 502:
-          //     break;
-          //   default:
-          //     break;
-          // }
-          // Loading.error(errorMessage.message);
+          final response = err.response;
+          final errorMessage = ErrorMessageModel.fromJson(response?.data);
+          switch (errorMessage.statusCode) {
+            case 401:
+              _errorNoAuthLogout();
+              break;
+            case 404:
+              break;
+            case 500:
+              break;
+            case 502:
+              break;
+            default:
+              break;
+          }
+          Loading.error(errorMessage.message);
         }
         break;
       case DioExceptionType.unknown:
