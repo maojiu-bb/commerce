24ef08152b2507e343564d99981e9d0cdb2dfea2
maojiu <maojiu-bb@outlook.com>
2023-08-15 21:20:42 +0800

    feat -> 搜索框

--- lib/pages/goods/home/controller.dart
+++ lib/pages/goods/home/controller.dart
@@ -3,6 +3,9 @@ import 'package:get/get.dart';
 class HomeController extends GetxController {
   HomeController();
 
+  // 导航点击事件
+  void onAppBarTap() {}
+
   _initData() {
     update(["home"]);
   }
