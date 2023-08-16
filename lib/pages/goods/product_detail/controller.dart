e575d4229927e664177ee3fa5da4b3b8c41a6d84
maojiu <maojiu-bb@outlook.com>
2023-08-16 23:39:24 +0800

    feat -> tab商品评论

--- lib/pages/goods/product_detail/controller.dart
+++ lib/pages/goods/product_detail/controller.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:woo_commerce/common/index.dart';
 
 class ProductDetailController extends GetxController
@@ -31,6 +32,19 @@ class ProductDetailController extends GetxController
   List<KeyValueModel<AttributeModel>> sizes = [];
   // 选中尺寸列表
   List<String> sizeKeys = [];
+
+  // 评论 刷新控制器
+  final RefreshController reviewsRefreshController = RefreshController(
+    initialRefresh: true,
+  );
+  // reviews 评论列表
+  List<ReviewModel> reviews = [];
+  // 评论图片列表 测试用
+  List<String> reviewImages = [];
+  // 评论 页码
+  int _reviewsPage = 1;
+  // 评论 页尺寸
+  final int _reviewsLimit = 20;
 
   // 读取缓存
   _loadCache() async {
@@ -85,6 +99,48 @@ class ProductDetailController extends GetxController
         sizeKeys = sizeAttr?.first.options ?? [];
       }
     }
+
+    // 评论
+    reviews = await ProductApi.reviews(ReviewsReq(
+      product: productId,
+    ));
+
+    // 评论图片，测试用
+    reviewImages.addAll([
+      "https://ducafecat.oss-cn-beijing.aliyuncs.com/bag/718Y%2BhJkMgL._AC_UY695_.jpg",
+      "https://ducafecat.oss-cn-beijing.aliyuncs.com/bag/71n8Tg2ClZL._AC_UY695_.jpg",
+      "https://ducafecat.oss-cn-beijing.aliyuncs.com/bag/819mEKajDML._AC_UY695_.jpg",
+      "https://ducafecat.oss-cn-beijing.aliyuncs.com/bag/81J0UFuJHdL._AC_UY695_.jpg",
+      "https://ducafecat.oss-cn-beijing.aliyuncs.com/bag/81M4BxGW4TL._AC_UY695_.jpg",
+      "https://ducafecat.oss-cn-beijing.aliyuncs.com/bag/81s6OXEsZCL._AC_UY695_.jpg",
+    ]);
+  }
+
+  // 评论 拉取数据
+  Future<bool> _loadReviews(bool isRefresh) async {
+    // 拉取数据
+    // 评论
+    var reviewsListTmp = await ProductApi.reviews(ReviewsReq(
+      // 刷新, 重置页数1
+      page: isRefresh ? 1 : _reviewsPage,
+      // 每页条数
+      prePage: _reviewsLimit,
+      // 商品id
+      product: productId,
+    ));
+
+    // 更新数据
+    if (isRefresh) {
+      _reviewsPage = 1; // 重置页数1
+      reviews.clear(); // 清空数据
+    }
+
+    if (reviewsListTmp.isNotEmpty) {
+      _reviewsPage++; // 页数+1
+      reviews.addAll(reviewsListTmp); // 添加数据
+    }
+
+    return reviewsListTmp.isEmpty;
   }
 
   // Banner 切换事件
@@ -118,6 +174,54 @@ class ProductDetailController extends GetxController
   void onSizeTap(List<String> keys) {
     sizeKeys = keys;
     update(["product_sizes"]);
+  }
+
+  // 评论 下拉刷新
+  void onReviewsRefresh() async {
+    try {
+      // 拉取数据是否为空
+      await _loadReviews(true);
+
+      // 刷新完成
+      reviewsRefreshController.refreshCompleted();
+    } catch (error) {
+      // 刷新失败
+      reviewsRefreshController.refreshFailed();
+    }
+    update(["product_reviews"]);
+  }
+
+  // 评论 上拉载入新商品
+  void onReviewsLoading() async {
+    if (reviews.isNotEmpty) {
+      try {
+        // 拉取数据是否为空
+        var isEmpty = await _loadReviews(false);
+
+        if (isEmpty) {
+          // 设置无数据
+          reviewsRefreshController.loadNoData();
+        } else {
+          // 加载完成
+          reviewsRefreshController.loadComplete();
+        }
+      } catch (e) {
+        // 加载失败
+        reviewsRefreshController.loadFailed();
+      }
+    } else {
+      // 设置无数据
+      reviewsRefreshController.loadNoData();
+    }
+    update(["product_reviews"]);
+  }
+
+  // 评论图片浏览
+  void onReviewsGalleryTap(int index) {
+    Get.to(GalleryWidget(
+      initialIndex: index,
+      items: reviewImages,
+    ));
   }
 
   _initData() async {
@@ -154,5 +258,7 @@ class ProductDetailController extends GetxController
   void onClose() {
     super.onClose();
     tabController.dispose();
+    // 释放 评论下拉控制器
+    reviewsRefreshController.dispose();
   }
 }
--- lib/pages/goods/product_detail/widgets/tab_reviews.dart
+++ lib/pages/goods/product_detail/widgets/tab_reviews.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:woo_commerce/common/index.dart';
 
 import '../index.dart';
 
@@ -12,8 +15,93 @@ class TabReviewsView extends GetView<ProductDetailController> {
   @override
   String? get tag => uniqueTag;
 
+  // 评论图
+  Widget _buildReviewImages() {
+    return <Widget>[
+      // 图
+      for (var i = 0; i < controller.reviewImages.length; i++)
+        ImageWidget.url(
+          controller.reviewImages[i],
+          width: 45.w,
+          height: 45.w,
+        ) // 图片浏览
+            .onTap(() => controller.onReviewsGalleryTap(i))
+            .paddingRight(AppSpace.listItem),
+    ].toWrap(
+      spacing: AppSpace.page,
+      runSpacing: AppSpace.listItem,
+    );
+  }
+
+  // 列表项
+  _buildListItem(ReviewModel item) {
+    return <Widget>[
+      // 头像
+      const ImageWidget.asset(
+        AssetsImages.defaultPng,
+        width: 55,
+        height: 55,
+      ).paddingRight(AppSpace.listItem),
+
+      // 星、名称、评论、图
+      <Widget>[
+        // 名称、时间
+        <Widget>[
+          // 名称
+          TextWidget.title3(
+            item.reviewer ?? "",
+          ).expanded(),
+          // 时间
+          TextWidget.body2(
+            '${item.dateCreated?.split('T')[0]}   ${item.dateCreated?.split('T')[1]}',
+          ),
+        ].toRow(),
+        // 评论
+        TextWidget.body1(
+          item.review?.clearHtml ?? "",
+        ),
+        // 图
+        _buildReviewImages(),
+      ]
+          .toColumn(
+            crossAxisAlignment: CrossAxisAlignment.start,
+          )
+          .expanded(),
+    ].toRow(
+      crossAxisAlignment: CrossAxisAlignment.start,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text("评论");
+    return GetBuilder<ProductDetailController>(
+      tag: tag,
+      id: "product_reviews",
+      builder: (_) {
+        return SmartRefresher(
+          // 刷新控制器
+          controller: controller.reviewsRefreshController,
+          // 启用上拉加载
+          enablePullUp: true,
+          // 下拉刷新回调
+          onRefresh: controller.onReviewsRefresh,
+          // 上拉加载回调
+          onLoading: controller.onReviewsLoading,
+          // 底部加载更多
+          footer: const SmartRefresherFooterWidget(),
+          // 分隔符、间距
+          child: ListView.separated(
+            itemBuilder: (BuildContext context, int index) {
+              var item = controller.reviews[index];
+              return _buildListItem(item);
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return SizedBox(height: AppSpace.listRow * 2);
+            },
+            itemCount: controller.reviews.length,
+          ),
+        );
+      },
+    );
   }
 }
