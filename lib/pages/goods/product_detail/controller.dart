aa538f0183c26a702ac6c65444c7b70aa3b4d19a
maojiu <maojiu-bb@outlook.com>
2023-08-16 16:12:46 +0800

    feat -> 商品详情页轮播图

--- lib/pages/goods/product_detail/controller.dart
+++ lib/pages/goods/product_detail/controller.dart
@@ -1,9 +1,43 @@
 import 'package:get/get.dart';
+import 'package:woo_commerce/common/index.dart';
 
 class ProductDetailController extends GetxController {
   ProductDetailController();
 
-  _initData() {
+  // 商品 id , 获取路由传递参数
+  int? productId = Get.arguments['id'] ?? 0;
+  // 商品详情
+  ProductModel? product;
+  // Banner 数据
+  List<KeyValueModel> bannerItems = [];
+  // Banner 当前位置
+  int bannerCurrentIndex = 0;
+
+  // 拉取商品详情
+  _loadProduct() async {
+    // 商品详情
+    product = await ProductApi.productDetail(productId);
+
+    // Banner 数据
+    if (product?.images != null) {
+      bannerItems = product!.images!
+          .map<KeyValueModel>((e) => KeyValueModel(
+                key: "${e.id}",
+                value: e.src ?? "",
+              ))
+          .toList();
+    }
+  }
+
+  // Banner 切换事件
+  void onChangeBanner(int index, reason) {
+    bannerCurrentIndex = index;
+    update(["product_banner"]); // 手动刷新 Banner
+  }
+
+  _initData() async {
+    await _loadProduct();
+
     update(["product_detail"]);
   }
 
--- lib/pages/goods/product_detail/view.dart
+++ lib/pages/goods/product_detail/view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:woo_commerce/common/index.dart';
 
@@ -40,7 +41,27 @@ class _ProductDetailViewGetX extends GetView<ProductDetailController> {
 
 // 滚动图
   Widget _buildBanner() {
-    return const Text("滚动图");
+    return GetBuilder<ProductDetailController>(
+        id: "product_banner",
+        tag: tag,
+        builder: (_) {
+          return CarouselWidget(
+            // 图片列表
+            items: controller.bannerItems,
+            // 当前索引
+            currentIndex: controller.bannerCurrentIndex,
+            // 切换回调
+            onPageChanged: controller.onChangeBanner,
+            // 高度
+            height: 190.w,
+            // 指示器圆点
+            indicatorCircle: false,
+            // 指示器位置
+            indicatorAlignment: MainAxisAlignment.start,
+            // 指示器颜色
+            indicatorColor: AppColors.highlight,
+          );
+        }).backgroundColor(AppColors.surfaceVariant);
   }
 
   // 商品标题
@@ -60,19 +81,21 @@ class _ProductDetailViewGetX extends GetView<ProductDetailController> {
 
   // 主视图
   Widget _buildView() {
-    return <Widget>[
-      // 滚动图
-      _buildBanner(),
+    return controller.product == null
+        ? const PlaceholdWidget() // 占位图
+        : <Widget>[
+            // 滚动图
+            _buildBanner(),
 
-      // 商品标题
-      _buildTitle(),
+            // 商品标题
+            _buildTitle(),
 
-      // Tab 栏位
-      _buildTabBar(),
+            // Tab 栏位
+            _buildTabBar(),
 
-      // TabView 视图
-      _buildTabView(),
-    ].toColumn();
+            // TabView 视图
+            _buildTabView(),
+          ].toColumn();
   }
 
   @override
@@ -85,7 +108,9 @@ class _ProductDetailViewGetX extends GetView<ProductDetailController> {
       builder: (_) {
         return Scaffold(
           // 导航
-          appBar: mainAppBarWidget(titleString: LocaleKeys.gDetailTitle.tr),
+          appBar: mainAppBarWidget(
+            titleString: controller.product?.name ?? LocaleKeys.gDetailTitle.tr,
+          ),
           // 内容
           body: SafeArea(
             child: _buildView(),
