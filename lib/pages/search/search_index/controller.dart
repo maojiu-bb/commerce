89b06573ef1018791dd92166dfcc1272d2c65e8a
maojiu <maojiu-bb@outlook.com>
2023-08-17 10:01:06 +0800

    feat -> 推荐关键词列表

--- lib/pages/search/search_index/controller.dart
+++ lib/pages/search/search_index/controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:woo_commerce/common/index.dart';
 
 class SearchIndexController extends GetxController {
   SearchIndexController();
@@ -10,6 +11,9 @@ class SearchIndexController extends GetxController {
 
   // 搜索关键词
   final searchKeyWord = "".obs;
+
+  // Tags 列表
+  List<TagsModel> tagsList = [];
 
   // 搜索栏位 - 防抖
   void searchDebounce() {
@@ -26,11 +30,12 @@ class SearchIndexController extends GetxController {
         }
 
         // 拉取数据
+        await _loadSearch(value);
         update(["search_index"]);
       },
 
-      // 延迟 500 毫秒
-      time: const Duration(milliseconds: 500),
+      // 延迟 16 毫秒
+      time: const Duration(milliseconds: 16),
     );
 
     // 监听搜索框变化
@@ -38,6 +43,33 @@ class SearchIndexController extends GetxController {
       searchKeyWord.value = searchEditController.text;
     });
   }
+
+  /// 拉取数据
+  Future<bool> _loadSearch(String keyword) async {
+    if (keyword.trim().isEmpty == true) {
+      tagsList.clear();
+      return tagsList.isEmpty;
+    }
+
+    // 拉取数据
+    var result = await ProductApi.tags(TagsReq(
+      // 关键词
+      search: keyword,
+    ));
+
+    // 清空数据
+    tagsList.clear();
+
+    // 返回数据不为空
+    if (result.isNotEmpty) {
+      tagsList.addAll(result); // 添加数据
+    }
+
+    return tagsList.isEmpty;
+  }
+
+  // 列表项点击事件
+  void onListItemTap(TagsModel model) {}
 
   _initData() {
     update(["search_index"]);
