0bed4986f25b9e65435c2f41ffb87fa1498a971b
maojiu <maojiu-bb@outlook.com>
2023-08-17 15:46:15 +0800

    feat -> 拉取商品sliver滚动

--- lib/pages/search/search_filter/widgets/filter_view.dart
+++ lib/pages/search/search_filter/widgets/filter_view.dart
@@ -193,6 +193,16 @@ class FilterView extends GetView<SearchFilterController> {
       // 条件选择
       _buildTitle(LocaleKeys.searchFilterCondition.tr),
       _buildConditions(),
+
+      // 应用按钮
+      const Divider(),
+      ButtonWidget.primary(
+        LocaleKeys.commonBottomApply.tr,
+        onTap: controller.onFilterApplyTap,
+      ).tight(
+        width: 75,
+        height: 25,
+      ),
     ]
         .toColumn(
           crossAxisAlignment: CrossAxisAlignment.start,
