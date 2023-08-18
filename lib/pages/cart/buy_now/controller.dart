0dc55fcde626ca3585483faf9886333e052dc93c
maojiu <maojiu-bb@outlook.com>
2023-08-18 22:25:52 +0800

    feat -> 优惠券栏位

--- lib/pages/cart/buy_now/controller.dart
+++ lib/pages/cart/buy_now/controller.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:woo_commerce/common/index.dart';
+import 'package:woo_commerce/pages/index.dart';
 
 class BuyNowController extends GetxController {
   BuyNowController(this.product);
@@ -22,10 +25,17 @@ class BuyNowController extends GetxController {
   int quantity = 1;
   // 运费
   double get shipping => 0;
-  // 折扣
-  double get discount => 0;
   // 商品合计价格
   double get totalPrice => double.parse(product.price!) * quantity;
+
+  // 优惠券列表
+  final List<CouponsModel> lineCoupons = [];
+
+  // 折扣
+  double get discount =>
+      lineCoupons.fold<double>(0, (double previousValue, CouponsModel element) {
+        return previousValue + (double.parse(element.amount ?? "0"));
+      });
 
   // 修改数量
   void onQuantityChange(int value) {
@@ -54,6 +64,47 @@ class BuyNowController extends GetxController {
       shippingAddress = UserService.to.shipping;
       update(["buy_now"]);
     }
+  }
+
+  // 使用优惠券
+  bool _applyCoupon(CouponsModel item) {
+    // 是否有重复
+    int index = lineCoupons.indexWhere((element) => element.id == item.id);
+    if (index >= 0) {
+      return false;
+    }
+    // 添加
+    lineCoupons.add(item);
+    return true;
+  }
+
+  // 显示输入优惠券 568935ab
+  void onEnterCouponCode() {
+    ActionBottomSheet.popModal(
+      child: ApplyPromoCodePage(
+        onApplyCouponCode: (couponCode) async {
+          // 判断优惠券是否存在
+          if (couponCode.isEmpty) {
+            Loading.error("Voucher code empty.");
+            return;
+          }
+          CouponsModel? coupon = await CouponApi.couponDetail(couponCode);
+          if (coupon != null) {
+            couponCode = "";
+            bool isSuccess = _applyCoupon(coupon);
+            if (isSuccess) {
+              Loading.success("Coupon applied.");
+            } else {
+              Loading.error("Coupon is already applied.");
+            }
+            update(["buy_now"]);
+          } else {
+            Loading.error("Coupon code is not valid.");
+          }
+        },
+      ),
+      safeAreaMinimum: EdgeInsets.fromLTRB(50.w, 0, 50.w, 140.w),
+    );
   }
 
   // @override
