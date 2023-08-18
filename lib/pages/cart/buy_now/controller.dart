6c47650fcbee064245c1487ff3cfa1eab26b9278
maojiu <maojiu-bb@outlook.com>
2023-08-18 18:23:48 +0800

    feat -> 送货地址栏位

--- lib/pages/cart/buy_now/controller.dart
+++ lib/pages/cart/buy_now/controller.dart
@@ -15,14 +15,28 @@ class BuyNowController extends GetxController {
     AssetsImages.pPaypalPng,
   ];
 
+  // 送货地址
+  String shippingAddress = "";
+
   // 下单 checkout
   void onCheckout() async {}
 
   _initData() {
+    // 获取送货地址
+    shippingAddress = UserService.to.shipping;
+
     update(["buy_now"]);
   }
 
-  void onTap() {}
+  // goto 送货地址修改
+  Future<void> onShippingTap() async {
+    var result = await Get.toNamed(RouteNames.myMyAddress,
+        arguments: {"type": "Shipping"});
+    if (result != null && result == true) {
+      shippingAddress = UserService.to.shipping;
+      update(["buy_now"]);
+    }
+  }
 
   // @override
   // void onInit() {
