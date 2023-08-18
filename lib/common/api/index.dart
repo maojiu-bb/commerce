9cf9d09fc887e3c4161ed6196bfdf518b8e9b02a
maojiu <maojiu-bb@outlook.com>
2023-08-18 22:46:51 +0800

    feat -> 下单成功界面

--- lib/common/api/index.dart
+++ lib/common/api/index.dart
@@ -4,4 +4,5 @@ export './user.dart';
 export './system.dart';
 export './product.dart';
 export './coupon.dart';
+export './order.dart';
 // export './xxxx.dart';
--- lib/pages/cart/buy_now/controller.dart
+++ lib/pages/cart/buy_now/controller.dart
@@ -46,9 +46,6 @@ class BuyNowController extends GetxController {
     update(["buy_now"]);
   }
 
-  // 下单 checkout
-  void onCheckout() async {}
-
   _initData() {
     // 获取送货地址
     shippingAddress = UserService.to.shipping;
@@ -105,6 +102,32 @@ class BuyNowController extends GetxController {
       ),
       safeAreaMinimum: EdgeInsets.fromLTRB(50.w, 0, 50.w, 140.w),
     );
+  }
+
+  // 下单 checkout
+  void onCheckout() async {
+    // 商品 LineItem
+    List<LineItem> lineItems = [
+      LineItem(
+        productId: product.id,
+        quantity: quantity,
+      ),
+    ];
+
+    // 提交订单
+    OrderModel res = await OrderApi.crateOrder(
+      lineItem: lineItems,
+      lineCoupons: lineCoupons,
+    );
+
+    // 交易成功
+    if (res.id != null) {
+      // 提示
+      Loading.success("Order created.");
+
+      // goto 成功界面
+      Get.offNamed(RouteNames.cartBuyDone, arguments: res);
+    }
   }
 
   // @override
