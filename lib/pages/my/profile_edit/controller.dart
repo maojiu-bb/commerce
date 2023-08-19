5d312bfbb7d44f9ec92c338e30c1a04db26dfa62
maojiu <maojiu-bb@outlook.com>
2023-08-19 16:20:49 +0800

    feat -> 修改我的信息页面

--- lib/pages/my/profile_edit/controller.dart
+++ lib/pages/my/profile_edit/controller.dart
@@ -1,9 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:woo_commerce/common/index.dart';
 
 class ProfileEditController extends GetxController {
   ProfileEditController();
 
+  // 表单 form
+  GlobalKey formKey = GlobalKey<FormState>();
+
+  // 输入框控制器
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController oldPasswordController = TextEditingController();
+  TextEditingController newPasswordController = TextEditingController();
+  TextEditingController confirmNewPasswordController = TextEditingController();
+
+  // 初始数据_
   _initData() {
+    // 用户 profile
+    UserProfileModel profile = UserService.to.profile;
+
+    // 初始值
+    firstNameController.text = profile.firstName ?? "";
+    lastNameController.text = profile.lastName ?? "";
+    emailController.text = profile.email ?? "";
     update(["profile_edit"]);
   }
 
@@ -20,8 +41,15 @@ class ProfileEditController extends GetxController {
     _initData();
   }
 
-  // @override
-  // void onClose() {
-  //   super.onClose();
-  // }
+  @override
+  void onClose() {
+    super.onClose();
+    // 控制器释放
+    firstNameController.dispose();
+    lastNameController.dispose();
+    emailController.dispose();
+    oldPasswordController.dispose();
+    newPasswordController.dispose();
+    confirmNewPasswordController.dispose();
+  }
 }
